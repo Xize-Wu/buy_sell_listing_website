ba77acbfda8f969816214d02dc22ac4c2b617b88
Wendina <wendina.teng@gmail.com>
2023-04-09 22:02:41 +0000

    Work on client facing side files.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,31 @@
 // Client facing scripts here
+$(() => {
+  const $productListings = $(`
+  <section class="product-listings" id="product-listings">
+      <p>Loading...</p>
+    </section>
+  `);
+  window.$productListings = $productListings;
+
+  window.productListings = {};
+
+  function addListing(listing) {
+    $productListings.append(listing);
+  }
+  function clearListings() {
+    $productListings.empty();
+  }
+  window.productListings.clearListings = clearListings;
+
+  function addProducts(products) {
+    clearListings();
+    for (const productId in products) {
+      const product = products[product];
+      const listing = $productListings.createListing($productListings);
+      addListing(listing);
+    }
+  }
+  window.productListings.addProperties = addProperties;
+});
+// HEADER
+
