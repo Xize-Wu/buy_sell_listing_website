4168fc9143a0024b341950f32d15faa2e8f25215
Wendina <wendina.teng@gmail.com>
2023-04-09 21:33:46 +0000

    Add sql query to get all products.

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,21 @@ const getUsers = () => {
     });
 };
 
-module.exports = { getUsers };
+const getAllProducts = (options, limit = 10) => {
+  return pool.query(`
+  SELECT users.name, title, picture_url, price, condition, category, products.created_at as posted_time
+  FROM products
+  JOIN users ON user_id = users.id
+  GROUP BY users.name, title, picture_url, price, condition, category, posted_time
+  ORDER BY posted_time DESC;
+  `)
+  .then ((result) => {
+    console.log(result);
+    return result;
+  })
+  .catch ((error) => {
+    console.log(error);
+  })
+};
+
+module.exports = { getUsers, getAllProducts };
