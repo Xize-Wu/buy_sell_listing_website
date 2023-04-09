0e5fcfdd58706d87cb833bc801ab6f71e4db38e6
Wendina <wendina.teng@gmail.com>
2023-04-09 18:37:43 +0000

    Modify sql file.

--- db/queries/05_get_messages_by_session_id.sql
+++ db/queries/05_get_messages_by_session_id.sql
@@ -1,9 +1,9 @@
 -- show list of messages by session_id for each user --> goes on the sessions page
 
-SELECT session_id, products.title, users.name as customer_name, seller_id, messages.id, messages.message, messages.created_at as message_time_sent
+SELECT sessions.id, products.title, sessions.customer_id, users.name as customer_name, sessions.seller_id, messages.id, messages.created_at as message_time_sent
 FROM sessions
 JOIN messages ON session_id = sessions.id
 JOIN products ON product_id = products.id
 JOIN users ON user_id = users.id
-GROUP BY session_id, products.title, customer_name, seller_id, messages.id, messages.message
+GROUP BY sessions.id, products.title, sessions.customer_id, customer_name, sessions.seller_id, messages.id
 ORDER BY message_time_sent;
