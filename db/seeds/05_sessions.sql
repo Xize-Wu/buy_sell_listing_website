de2b7e82dae9bf13773d65c703717aa305bfac55
Wendina <wendina.teng@gmail.com>
2023-04-09 05:13:05 +0000

    Add fake data for '05_msessions' and '06_messages' seed files.

--- db/seeds/05_sessions.sql
+++ db/seeds/05_sessions.sql
@@ -1,5 +1,8 @@
 -- Fake data template
 INSERT INTO sessions (customer_id, product_id, seller_id)
 VALUES(1, 5, (SELECT products.user_id FROM products
-WHERE products.id = 5));
-
+      WHERE products.id = 5)),
+      (3, 2, (SELECT products.user_id FROM products
+      WHERE products.id = 2)),
+      (2, 4, (SELECT products.user_id FROM products
+      WHERE products.id = 4));
