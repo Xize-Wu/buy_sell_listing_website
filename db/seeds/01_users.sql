b96a120e5465030d1994a0d2831c6f04d2314a7e
Wendina <wendina.teng@gmail.com>
2023-04-09 03:20:54 +0000

    Change file name order by numbering them appropriately. Also update file '01_users' to clean up SQL. And add fake data into file '02_products'.

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, email, password, admin)
+VALUE ('Alice', 'aliceinwonderland@xmail.com', 'password1', FALSE),
+('Gandalf', 'thewizard@middleearth.net', 'password2', TRUE),
+('Draco_Malfoy', 'potterstinks@hogwarts.edu', 'password3', FALSE),
+('Murphy', 'lordofdreams@endless.com', 'password4', TRUE),
+('Laenor_V', 'sooooogay@driftmark.wt', 'password5', FALSE);
