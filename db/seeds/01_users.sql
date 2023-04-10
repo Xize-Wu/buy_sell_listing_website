ab57ea61b2119e20df2e5367ee760ed5fb3566b8
Wendina <wendina.teng@gmail.com>
2023-04-10 21:09:12 +0000

    Modify the passwords to a hashed version, the passwords are all 'password2'.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, email, password, admin)
-VALUES ('Alice', 'aliceinwonderland@xmail.com', 'password1', FALSE),
-('Gandalf', 'thewizard@middleearth.net', 'password2', TRUE),
-('Draco_Malfoy', 'potterstinks@hogwarts.edu', 'password3', FALSE),
-('Murphy', 'lordofdreams@endless.com', 'password4', TRUE),
-('Laenor_V', 'sooooogay@driftmark.wt', 'password5', FALSE);
+VALUES ('Alice', 'aliceinwonderland@xmail.com', '$2a$10$shpXSmRtRXVh/H2B3jUmyOjOLavfMhnehONFLjNchu0x7jYRMVGii', FALSE),
+('Gandalf', 'thewizard@middleearth.net', '$2a$10$shpXSmRtRXVh/H2B3jUmyOjOLavfMhnehONFLjNchu0x7jYRMVGii', TRUE),
+('Draco_Malfoy', 'potterstinks@hogwarts.edu', '$2a$10$shpXSmRtRXVh/H2B3jUmyOjOLavfMhnehONFLjNchu0x7jYRMVGii', FALSE),
+('Murphy', 'lordofdreams@endless.com', '$2a$10$shpXSmRtRXVh/H2B3jUmyOjOLavfMhnehONFLjNchu0x7jYRMVGii', TRUE),
+('Laenor_V', 'sooooogay@driftmark.wt', '$2a$10$shpXSmRtRXVh/H2B3jUmyOjOLavfMhnehONFLjNchu0x7jYRMVGii', FALSE);
