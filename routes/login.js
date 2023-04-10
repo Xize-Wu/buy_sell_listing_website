6682a3cde2c239a513931764c9d5d6a2c62d3d47
Wendina <wendina.teng@gmail.com>
2023-04-10 16:35:12 +0000

    Modify the login post router

--- routes/login.js
+++ routes/login.js
@@ -2,22 +2,19 @@ const express = require('express');
 const userQueries = require('../db/queries/users');
 
 // Do the cookiesession/bcrypt/salt go in this file? or in the server file?
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
-const salt = bcrypt.genSaltSync(10);
+const bcrypt = require('bcryptjs');
+// const salt = bcrypt.genSaltSync(10);
 
 // in the seed file?? to has the password! password = brcrypt.hashSync(process.env.USER1_PASSWORD, salt)
 // to authenticate user with bcrypt
 
-const samePassword = bcrypt.compareSync(password, result.password);
-if (!samePassword) {
-  return { error: `Password dpes not match`, user: null}
-}
+
 
 const router  = express.Router();
 
-router.use(cookieParser());
+  // router.use(cookieParser());
 router.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
@@ -30,22 +27,21 @@ router.post('/', (req, res) => {
 
   userQueries.getUserWithEmail(email)
   .then((result) => {
-    const user = result.email;
 
     // AUTHENTICATE USER --> also have to use bcrypt, but does it go to its own helper function "authenticateUser" outside of the router?
-    if (!user) {
+    if (result === undefined) {
       console.log(`Error: User doesn't exist`);
       return res.redirect('/');
     }
 
-    if (result.password !== password) {
+    const samePassword = bcrypt.compareSync(password, result.password);
+    if (!samePassword) {
       console.log(`Error: Password doesn't match`);
       return res.redirect('/');
     }
 
-    // DOES REQ.SESSION GO HERE? OR OUTSIDE OF THE userQueries portion?
-    // req.session = user;
-    // console.log(`what is this? REQ SESSION: `, req.session);
+
+    req.session.user_id = result.id;
     res.redirect('/');
   })
   .catch((error) => {
@@ -53,8 +49,6 @@ router.post('/', (req, res) => {
     // res.status(400).send('Invalid email: ', error.message);
   })
 
-
-  //// Does req session go here?
 });
 
 //Login GET route
