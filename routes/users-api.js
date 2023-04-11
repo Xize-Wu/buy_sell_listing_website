3f206a8c008f90fa6f37aa74baaa069483a25861
Wendina <wendina.teng@gmail.com>
2023-04-11 16:00:24 +0000

    Add new GET route for '/favourites', it calls getAllFavourites function to retrieve the necessary data and passes that data into the templateVars that gets rendered to the 'favourites' template.

--- routes/users-api.js
+++ routes/users-api.js
@@ -51,6 +51,31 @@ router.get('/orders', (req, res) => {
       .status(500)
       .json({ error: err.message });
   })
-})
+});
+
+router.get('/favourites', (req, res) => {
+  const user_id = req.session.userId;
+
+  userQueries.getAllFavourites(user_id)
+  .then(favourites => {
+
+    if (!user_id) {
+      return res.redirect('/')
+    }
+
+    const templateVars = {
+      favourites,
+      username: req.session.username
+    }
+
+    res.render('favourites', templateVars);
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  })
+});
+
 
 module.exports = router;
