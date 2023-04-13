5e8140e435961805246f402705596dfdaca9505b
Wendina <wendina.teng@gmail.com>
2023-04-13 05:50:02 +0000

    Clean up file, remove unncessary things, and pass in userId as parameter.

--- routes/my_listing_form.js
+++ routes/my_listing_form.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userQueries = require('../db/queries/users');
-const bcrypt = require('bcryptjs');
+// post the form and redirect back to home page and it should show the new product 
 // view the posting form
 const router = express.Router();
 
@@ -11,23 +11,28 @@ router.get('/myLists', (req, res) => {
 });
 
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const userId = req.session.userId;
+  console.log('THIS IS THE SESSION USERID: ', userId);
+
   if (!userId) {
-    return res.send({ error: "error" });
+    return res.redirect('/');
   }
 
   const newListing = req.body;
-  newListing.user_id = userId;
+
   userQueries
-    .addListing(newListing)
+    .addListing(userId, newListing)
     .then((products) => {
-      res.send(products);
+      res.send(200, { message: 'ok' });
+      // res.send(products);
     })
-    .catch((e) => {
-      console.error(e);
-      res.send(e);
+    .catch((error) => {
+      console.log(error.message);
+      res.send(error);
     });
+
+    res.redirect('/');
 });
 
 module.exports = router;
