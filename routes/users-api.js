751ac40ed567874fd49c6e007c5851274b9f3aea
Wendina <wendina.teng@gmail.com>
2023-04-13 00:07:49 +0000

    Modify the home route to merge the favourite products and general product listings for the user so they can see both their favourites AND regular products.

--- routes/users-api.js
+++ routes/users-api.js
@@ -12,14 +12,30 @@ const userQueries = require('../db/queries/users');
 // Home route
 router.get('/', (req, res) => {
   userQueries.getAllProducts()
-    .then(products => {
+    .then(productsResult => {
+      const user_id = req.session.userId;
 
-      const templateVars = {
-        products,
-        username: req.session.username
-      };
+      userQueries.getAllFavourites(user_id)
+      .then(favourites => {
+        const favouriteTracker = {};
 
-      res.render('index', templateVars);
+        // { product_id: 1}
+        // map between product id and favourite id.
+        // we get list of products and then we get list of user favourite products
+        //// combine both by assigning favourite id to product
+                                                        // 1                     1
+        favourites.forEach(favourite => favouriteTracker[favourite.product_id] = favourite.id)
+
+        // cloned the product, add favourite_id key and assigning that value to key
+        const products = productsResult.map(product => ({...product, favourite_id: favouriteTracker[product.id]}))
+
+        const templateVars = {
+          products,
+          username: req.session.username
+        };
+
+        res.render('index', templateVars);
+      })
     })
     .catch(err => {
       res
