16df3e22c38be9021afdfd1e7eab27f96bd28835
Wendina <wendina.teng@gmail.com>
2023-04-13 04:22:14 +0000

    Create new file for the add listing routes. Mount the routes in server.js

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const orderRoutes = require('./routes/orders');
 const favouritesRoutes = require('./routes/favourites');
 const listingsRoutes = require('./routes/listings');
 const searchRoutes = require('./routes/search');
+const newlistingRoutes = require('./routes/my_listing_form');
 // const markSoldRoutes = require('./routes/mark_sold')
 
 
@@ -65,9 +66,10 @@ app.use('/orders', orderRoutes);
 app.use('/favourites', favouritesRoutes);
 app.use('/listings', listingsRoutes);
 app.use('/search', searchRoutes);
+app.use('/my_listing_form', newlistingRoutes);
 // app.use('/mark_sold', markSoldRoutes)
 // Note: mount other resources here, using the same pattern above
-// 
+//
 
 // Home page
 // Warning: avoid creating more routes in this file!
