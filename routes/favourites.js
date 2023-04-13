80568188ef0e1484610030d5e1bb0e5232357eb3
Wendina <wendina.teng@gmail.com>
2023-04-13 00:05:29 +0000

    Add new post route to remove the product from the favourites page, it calls the function and passes in userId and productId.

--- routes/favourites.js
+++ routes/favourites.js
@@ -45,4 +45,24 @@ router.post('/', (req, res) => {
   })
 })
 
+//Remove book from favourites
+router.post('/remove', (req, res) => {
+  console.log(req.body)
+  const user_id = req.session.userId;
+  const product_id = req.body.product_id;
+
+  userQueries.removeProductFromFavourites(user_id, product_id)
+  .then(response => {
+    console.log(response.rows)
+    res.send(200, { message: 'ok' });
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  })
+
+  res.redirect('/favourites');
+})
+
 module.exports = router;
