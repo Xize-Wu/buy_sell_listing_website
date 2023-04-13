065d655eab03ccbb8897a82a83749c8d5ad5b96e
Wendina <wendina.teng@gmail.com>
2023-04-13 20:21:39 +0000

    Update route and tboth futh functions being called is tested and working well.

--- routes/mark_sold.js
+++ routes/mark_sold.js
@@ -1,38 +1,58 @@
 const express = require('express');
 const router = express.Router();
-const userQueries = require('../db/queries/users');
+const userQueries = require('../db/queries/markSold');
 
 
 router.post('/', (req, res) => {
-
-  console.log(req.body)
   // if logged user is NOT the user who posted the item, return error
-  const product_id = req.product_id;
+  const product_id = req.body.product_id;
   console.log("Product_ID", product_id);
 
-  const adminUser_id = userQueries.getAdminUserIdWithProduct(product_id);
-  console.log("adminUser_id:", adminUser_id);
+  userQueries.getAdminUserIdWithProduct(product_id)
+  .then (result => {
 
-  const user_id = req.session.userId;
+    const adminUserId = result.user_id;
+    console.log('THIS IS THE ADMIN USER ID!!, ', adminUserId);
 
-  if (adminUser_id !== user_id) {
-    const errorMessage = "User cannot mark sold";
-    res.render('/', { errorMessage });
-  }
+    const user_id = req.session.userId;
+    console.log('THIS IS THE CURRENT USER ID!: ', user_id);
 
-  userQueries.markSold(product_id)
-  .then(data => {
-    const templateVars = { data }
-    res.render('/', templateVars)
+    const templateVars = {
+      adminUserId,
+      user_id
+    }
+
+    res.render('show', templateVars);
+    // LAST STEP
+    // if (adminUserId !== user_id) {
+    //   /// THE BUTTON SHOULD NOT SHOW UP IF THIS IS THE CASE
+    // }
+
+    // happy path
+    // SQL query to update the product so available = false
+
+    userQueries.markSold(product_id)
+    .then(result => {
+
+      console.log('THIS IS THE RESULT FROM USERQUERY: ', result);
+      return result
+      // const templateVars = { data }
+      // res.render('show', templateVars);
+    })
+    .catch(error => {
+      console.message(error.message)
+    })
   })
-  .catch(error => {
-    console.message(error.message)
+  .catch (error => {
+    console.log(error);
   })
 
-  // happy path
-  // SQL query to update the product so available = false
-  // jQuery to show "mark as sold successful"
-  res.redirect('/');
+
+  // FIRST STEP - ENSURE TABLE PRODUCTS GET UPDATED [SUCCESS]
+  // STEP 2 - If updated, show "mark as sold successful"; jweury in public folder, .then(render)
+  // STEP 3-  show products as "mark sold" tag if products.available = FALSE;
+
+  res.redirect(`/show/${product_id}`);
 });
 
 module.exports = router;
