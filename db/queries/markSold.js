fa938f2c55642108a59f88ccfcbd94f704d5bfac
Wendina <wendina.teng@gmail.com>
2023-04-14 00:25:00 +0000

    Clean up file, took out a function to retrieve admin user id. This is no longer required for the mark_sold logic.

--- db/queries/markSold.js
+++ db/queries/markSold.js
@@ -1,20 +1,5 @@
 const db = require('../connection');
 
-
-// works in the terminal
-const getAdminUserIdWithProduct = function(productId) {
-  return db.query(`SELECT user_id FROM products WHERE id = $1`, [productId])
-  .then((result) => {
-
-    // console.log('THIS IS THE USERID WE WANT!!!:', result.rows[0]);
-    return result.rows[0];
-  })
-  .catch((error) => {
-    console.error(error.message);
-  })
-};
-
-// Need to test this!!
 const markSold = function(productId) {
   return db.query(`
   UPDATE products
@@ -31,8 +16,3 @@ const markSold = function(productId) {
 };
 
 module.exports = {getAdminUserIdWithProduct, markSold}
-
-
-/**'UPDATE products SET available = FALSE FROM products as prods
- * JOIN orders ON prods.id = orders.product_id
- * WHERE (orders.user_id = $1 AND orders.purchased = FALSE AND orders.removed = FALSE) returning *' */
