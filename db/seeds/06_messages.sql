1820d8a59b10d35da47997d12d89d4e52440ecc0
Wendina <wendina.teng@gmail.com>
2023-04-14 04:29:38 +0000

    Modify message seed and the console.log statement inside message.ejs.

--- db/seeds/06_messages.sql
+++ db/seeds/06_messages.sql
@@ -1,9 +1,9 @@
 INSERT INTO messages (session_id, sender_id, message)
-VALUES (1, 6, 'Hey Gandalf, why no gay books?'),
+VALUES (1, 5, 'Hey Gandalf, why no gay books?'),
        (1, 2, 'Sorry what?'),
-       (1, 6, 'Aren''t you literally Gandalf the Gay?'),
+       (1, 5, 'Aren''t you literally Gandalf the Gay?'),
        (1, 2, 'It was Gandalf the Grey. But now I am Gandalf the White, who has returned from death.'),
-       (1, 6, 'Disappointing. Maybe I''ll message that Ian McKellen fella, he looks hella gay.'),
+       (1, 5, 'Disappointing. Maybe I''ll message that Ian McKellen fella, he looks hella gay.'),
 
        (2, 1, 'Hello Mr. Wizard, do you deliver to the Wonderland?'),
        (2, 2, 'Sorry, delivery to Middle Earth only.'),
